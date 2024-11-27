a4d68b4c9fdb89b8c78828107abcd9864e0d621f
Necati CAN <nctiicn@gmail.com>
2024-11-27 18:18:54 +0300

    Button Component Created and Completed

---
+++ social-links-profile/src/components/ButtonBase.vue
@@ -0,0 +1,39 @@
+<script setup>
+// Props
+const props = defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+    href:{
+        type: String,
+    }
+})
+</script>
+<template>
+    <button type="button" class="button-base">
+        <a :href="props.href" class="button-base-link">{{props.label}}</a>
+    </button>
+</template>
+<style lang="css" scoped>
+.button-base{
+    width: 100%;
+    border: none;
+    background-color: var(--color-grey-700);
+    border-radius: 8px;
+}
+.button-base:hover{
+    background-color: var(--color-green);
+}
+.button-base:hover .button-base-link{
+    color: var(--color-grey-800);
+}
+.button-base-link{
+    padding: 14px 16px;
+    display: block;
+    color: rgba(255, 255, 255, .95);
+    font-size: 14px;
+    font-weight: 600;
+    letter-spacing: .4px;
+}
+</style>
