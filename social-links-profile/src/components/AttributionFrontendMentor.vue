fa904d31eacbe03391f912bdf4ad99391ba63238
Necati CAN <nctiicn@gmail.com>
2024-11-28 19:39:07 +0300

    Attribution Component Created and Style Files Edited

---
+++ social-links-profile/src/components/AttributionFrontendMentor.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded
+    by <a href="https://www.frontendmentor.io/profile/Ncticn">Necati CAN</a>.
+  </div>
+</template>
+<style lang="css" scoped>
+.attribution {
+  color: var(--color-white);
+  font-size: 11px;
+  text-align: center;
+  position: absolute;
+  bottom: 2%;
+  left: 0;
+  right: 0;
+  display: inline-block;
+}
+
+.attribution a {
+  color: hsl(228, 45%, 44%);
+}
+</style>
